9cc8dc750213dd8477d82ee55c41f2195de3148e
carissasnh <121391896+carissasnh@users.noreply.github.com>
2023-07-08 10:18:39 +0700

    Add files via upload

---
+++ post1.php
@@ -0,0 +1,17 @@
+<?php
+	for ($i = 1; $i <5; $i++){
+		echo "*";
+	for ($j = 0; $j < $i; $j++){
+		echo "*";	{ 
+	}
+	echo "<br>";
+	}
+	for ($i = 0; $i <5; $i++){
+		echo "*";
+	for ($j = 0; $j < $i; $j++){
+		echo "*";
+		}
+	echo "<br>";
+	}
+
+}?>
